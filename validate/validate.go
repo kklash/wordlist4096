6c7208811944fdb1ef6344e2fed3295625a01e35
kklash <k_klash@protonmail.com>
2023-12-07 06:00:31 -0800

    document the validate package

--- validate/validate.go
+++ validate/validate.go
@@ -1,3 +1,4 @@
+// Package validate contains wordlist validation rules.
 package validate
 
 import (
@@ -8,6 +9,7 @@ import (
 	"github.com/kklash/wordlist4096/damlev"
 )
 
+// DuplicateWords returns an error if the given word list contains any duplicates.
 func DuplicateWords(words []string) error {
 	allWordsSet := make(map[string]struct{})
 	for _, word := range words {
@@ -19,6 +21,8 @@ func DuplicateWords(words []string) error {
 	return nil
 }
 
+// WordLength returns an error if the given word is shorter than 3 characters
+// or longer than 8 characters.
 func WordLength(word string) error {
 	l := len(word)
 	if l < 3 {
@@ -30,6 +34,8 @@ func WordLength(word string) error {
 	return nil
 }
 
+// WordPrefix returns an error if the given word shares the same 4-character prefix
+// as another word in the word list.
 func WordPrefix(word string, allWords []string) error {
 	prefix := word
 	if len(word) >= 4 {
@@ -44,6 +50,8 @@ func WordPrefix(word string, allWords []string) error {
 	return nil
 }
 
+// WordMinDistance returns an error if the given word has a Damerau-Levenshtein distance of
+// less than 2 from any other word in the word list.
 func WordMinDistance(word string, words []string) error {
 	minDistance := len(word)
 	for _, otherWord := range words {
@@ -58,6 +66,7 @@ func WordMinDistance(word string, words []string) error {
 	return nil
 }
 
+// All runs all validation checks on a word list.
 func All(word string, currentWords []string) error {
 	if err := WordLength(word); err != nil {
 		return err
